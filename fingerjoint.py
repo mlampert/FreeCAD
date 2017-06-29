b7427cd73c74dbf1655de45d3e824fdb058cb46f
Markus Lampert <markus@bibi.ca>
2017-06-28 22:01:28 -0700

    Corrected selection and algorithm.

--- fingerjoint.py
+++ fingerjoint.py
@@ -28,7 +28,7 @@ class Joint:
         self.dim = dim
         self.offset = offset
         self.start = edge.Vertexes[0].Point
-        self.dir = edge.Vertexes[1].Point - edge.Vertexes[0].Point
+        self.dir = (edge.Vertexes[1].Point - edge.Vertexes[0].Point).normalize()
 
         # there's a possibility this will always be 1.0 ....
         self.scale = (edge.LastParameter - edge.FirstParameter) / edge.Length
@@ -41,18 +41,19 @@ class Joint:
         p0 = edge.valueAt(self.scale * (        offset - edge.FirstParameter))
         # the first side of the cutout is along the edge
         p1 = edge.valueAt(self.scale * (dim.x + offset - edge.FirstParameter))
-        # scond side is in the direction of the normal
+        # second side is in the opposite direction of the normal
         e2 = self.normal * dim.z
+        self.e2 = e2
 
-        p2 = p1 + e2
-        p3 = p0 + e2
+        p2 = p1 - e2
+        p3 = p0 - e2
 
         self.cutPoints = [p0,p1,p2,p3]
         self.cutWire = Part.makePolygon([p0,p1,p2,p3,p0])
         self.cutFace = Part.Face(self.cutWire)
 
-        v = Part.Vertex(self.normal)
-        v.rotate(Vector(), self.dir, -90)
+        v = Part.Vertex(self.dir)
+        v.rotate(Vector(), self.normal, -90)
 
         self.cutSolid = self.cutFace.extrude(v.Point * dim.y)
 
@@ -80,42 +81,52 @@ FreeCADGui.doCommand("from fingerjoint import Joint, display")
 FreeCADGui.doCommand("doc = FreeCAD.newDocument('finger-joint')")
 
 FreeCADGui.doCommand("sheet0 = doc.addObject('Part::Box', 'sheet0')")
-FreeCADGui.doCommand("sheet0.Width  = 200 # 20cm heigh")
-FreeCADGui.doCommand("sheet0.Height =   3 # 3mm thick")
-FreeCADGui.doCommand("sheet0.Length = 300 # 30cm wide")
-FreeCADGui.doCommand("sheet0.ViewObject.Transparency = 80")
-FreeCADGui.doCommand("sheet0.ViewObject.Visibility = False")
-
-FreeCADGui.doCommand("edge = Part.Edge(Part.LineSegment(Vector(297, 0, 3), Vector(297, 200, 3)))")
-FreeCADGui.doCommand("dim0 = FreeCAD.Vector(20, 3, 3)")
-
-FreeCADGui.doCommand("(faceId0,face0) = [f for f in enumerate(sheet0.Shape.Faces) if f[1].Surface.Axis == Vector(0,0,1) and f[1].Orientation == 'Reversed'][0]")
-FreeCADGui.doCommand("j0 = Joint()")
-FreeCADGui.doCommand("f0 = j0.featherSolid(sheet0.Shape, face0, edge, dim0, offset=10)")
-FreeCADGui.doCommand("display(f0, 'feather0')")
-
+sheet0 = FreeCAD.ActiveDocument.sheet0
+sheet0.Width  = 200 # 20cm heigh
+sheet0.Height =   3 # 3mm thick
+sheet0.Length = 300 # 30cm wide
+sheet0.ViewObject.Transparency = 80
+sheet0.ViewObject.ShapeColor = (random(), random(), random())
+FreeCADGui.doCommand("(faceId0,face0) = [f for f in enumerate(sheet0.Shape.Faces) if f[1].Surface.Axis == Vector(0,0,1) and f[1].Orientation != 'Reversed'][0]")
 
 FreeCADGui.doCommand("sheet1 = doc.addObject('Part::Box', 'sheet1')")
-FreeCADGui.doCommand("sheet1.Width  = 200")
-FreeCADGui.doCommand("sheet1.Height = 300")
-FreeCADGui.doCommand("sheet1.Length =   3")
-FreeCADGui.doCommand("sheet1.Placement = FreeCAD.Placement(Vector(297,0,0), FreeCAD.Rotation(Vector(0,0,1), 0))")
-FreeCADGui.doCommand("sheet1.ViewObject.Transparency = 80")
-FreeCADGui.doCommand("sheet1.ViewObject.Visibility = False")
+sheet1 = FreeCAD.ActiveDocument.sheet1
+sheet1.Width  = 200
+sheet1.Height = 300
+sheet1.Length =   3
+sheet1.Placement = FreeCAD.Placement(Vector(297,0,0), FreeCAD.Rotation(Vector(0,0,1), 0))
+sheet1.ViewObject.Transparency = 80
+sheet1.ViewObject.ShapeColor = (random(), random(), random())
+FreeCADGui.doCommand("(faceId1,face1) = [f for f in enumerate(sheet1.Shape.Faces) if f[1].Surface.Axis == Vector(1,0,0) and f[1].Orientation == 'Reversed'][0]")
 
-FreeCADGui.doCommand("dim1 = FreeCAD.Vector(20, -3, 3)")
+if True:
+    FreeCADGui.doCommand("edge = Part.Edge(Part.LineSegment(Vector(297, 0, 3), Vector(297, 200, 3)))")
+    FreeCADGui.doCommand("dim0 = FreeCAD.Vector(20, 3, 3)")
 
-FreeCADGui.doCommand("(faceId1,face1) = [f for f in enumerate(sheet0.Shape.Faces) if f[1].Surface.Axis == Vector(1,0,0) and f[1].Orientation != 'Reversed'][0]")
-FreeCADGui.doCommand("j1 = Joint()")
-FreeCADGui.doCommand("f1 = j1.featherSolid(sheet1.Shape, face1, edge, dim1, offset=-10)")
-FreeCADGui.doCommand("display(f1, 'feather1')")
+    FreeCADGui.doCommand("j0 = Joint()")
+    FreeCADGui.doCommand("f0 = j0.featherSolid(sheet0.Shape, face0, edge, dim0, offset=10)")
+    FreeCADGui.doCommand("display(f0, 'feather0')")
 
-FreeCAD.ActiveDocument.sheet0.ViewObject.ShapeColor = (random(), random(), random())
-FreeCAD.ActiveDocument.feather0.ViewObject.ShapeColor = FreeCAD.ActiveDocument.sheet0.ViewObject.ShapeColor
-FreeCAD.ActiveDocument.feather0.ViewObject.Transparency = 80
-FreeCAD.ActiveDocument.sheet1.ViewObject.ShapeColor = (random(), random(), random())
-FreeCAD.ActiveDocument.feather1.ViewObject.ShapeColor = FreeCAD.ActiveDocument.sheet1.ViewObject.ShapeColor
-FreeCAD.ActiveDocument.feather1.ViewObject.Transparency = 80
+    FreeCAD.ActiveDocument.feather0.ViewObject.ShapeColor = sheet0.ViewObject.ShapeColor
+    FreeCAD.ActiveDocument.feather0.ViewObject.Transparency = 80
+
+    FreeCADGui.doCommand("dim1 = FreeCAD.Vector(20, -3, 3)")
+
+    FreeCADGui.doCommand("j1 = Joint()")
+    FreeCADGui.doCommand("f1 = j1.featherSolid(sheet1.Shape, face1, edge, dim1, offset=-10)")
+    FreeCADGui.doCommand("display(f1, 'feather1')")
+
+    FreeCAD.ActiveDocument.feather1.ViewObject.ShapeColor = sheet1.ViewObject.ShapeColor
+    FreeCAD.ActiveDocument.feather1.ViewObject.Transparency = 80
+
+    sheet0.ViewObject.Visibility = False
+    sheet1.ViewObject.Visibility = False
+
+else:
+    FreeCADGui.Selection.clearSelection()
+    FreeCADGui.doCommand("Gui.Selection.addSelection(sheet0, 'Face%d' % (faceId0+1))")
+    FreeCADGui.doCommand("Gui.Selection.addSelection(sheet1, 'Face%d' % (faceId1+1))")
+
 
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
 FreeCADGui.ActiveDocument.ActiveView.viewAxonometric()
