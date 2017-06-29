03d93e355caf627b2b9ec26f0818d982f887dc39
Markus Lampert <markus@bibi.ca>
2017-06-29 10:29:39 -0700

    Checkpoint.

--- fingerjoint.py
+++ fingerjoint.py
@@ -71,9 +71,28 @@ class Joint:
         self.cutOuts = Part.makeCompound(self.cut)
         return solid.cut(self.cutOuts)
 
-    def makeBox(self, x, y, z):
-        s = Part.makeBox(20, 10, 1, Vector(0,0,0), Vector(x,y,z))
-        display(s, "box-%d%d%d" % (x,y,z))
+    def getSelection(self):
+        sel = FreeCADGui.Selection.getSelectionEx()
+        o0 = sel[0].Object
+        self.f00 = sel[0].SubObjects[0]
+        o1 = sel[1].Object
+        self.f10 = sel[1].SubObjects[0]
+
+        self.f0s = [f for f in o0.Shape.Faces if f.Surface.Axis == self.f00.Surface.Axis]
+        self.f1s = [f for f in o1.Shape.Faces if f.Surface.Axis == self.f10.Surface.Axis]
+        self.n0s = [f.Surface.Axis if f.Orientation == 'Forward' else f.Surface.Axis * -1 for f in self.f0s]
+        self.n1s = [f.Surface.Axis if f.Orientation == 'Forward' else f.Surface.Axis * -1 for f in self.f1s]
+
+        self.f01 = [f for f in o0.Shape.Faces if f.Surfaces.Axis == self.f00.Surface.Axis and f.Orientattion != self.f00.Orientation]
+        self.f11 = [f for f in o1.Shape.Faces if f.Surfaces.Axis == self.f10.Surface.Axis and f.Orientattion != self.f10.Orientation]
+
+        self.sections = []
+        for f0 in self.f0s:
+            for f1 in self.f1s:
+                self.sections.append(f0.seciont(f1))
+
+        return (o0, self.f00, o1, self.f10)
+
 
 FreeCADGui.doCommand("from FreeCAD import Vector")
 FreeCADGui.doCommand("from fingerjoint import Joint, display")
@@ -99,7 +118,7 @@ sheet1.ViewObject.Transparency = 80
 sheet1.ViewObject.ShapeColor = (random(), random(), random())
 FreeCADGui.doCommand("(faceId1,face1) = [f for f in enumerate(sheet1.Shape.Faces) if f[1].Surface.Axis == Vector(1,0,0) and f[1].Orientation == 'Reversed'][0]")
 
-if True:
+if False:
     FreeCADGui.doCommand("edge = Part.Edge(Part.LineSegment(Vector(297, 0, 3), Vector(297, 200, 3)))")
     FreeCADGui.doCommand("dim0 = FreeCAD.Vector(20, 3, 3)")
 
@@ -124,9 +143,16 @@ if True:
 
 else:
     FreeCADGui.Selection.clearSelection()
+    # Let's select the 2 outside edges, that do not intersect at all
+    FreeCADGui.doCommand("(faceId0,face0) = [f for f in enumerate(sheet0.Shape.Faces) if f[1].Surface.Axis == Vector(0,0,1) and f[1].Orientation == 'Reversed'][0]")
+    FreeCADGui.doCommand("(faceId1,face1) = [f for f in enumerate(sheet1.Shape.Faces) if f[1].Surface.Axis == Vector(1,0,0) and f[1].Orientation != 'Reversed'][0]")
     FreeCADGui.doCommand("Gui.Selection.addSelection(sheet0, 'Face%d' % (faceId0+1))")
     FreeCADGui.doCommand("Gui.Selection.addSelection(sheet1, 'Face%d' % (faceId1+1))")
 
+    FreeCADGui.doCommand("j = Joint()")
+    FreeCADGui.doCommand("(o0, f0, o1, f1)  = j.getSelection()")
+    #FreeCADGui.doCommand("(f0, f1, e) = j.findFacesAndEdge(o0, f0, o1, f1))")
+    FreeCADGui.doCommand("display(f0.section(f1), 'intersection')")
 
 FreeCADGui.ActiveDocument.ActiveView.fitAll()
 FreeCADGui.ActiveDocument.ActiveView.viewAxonometric()
